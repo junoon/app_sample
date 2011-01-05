7157b77ff6a11bd9a5d52ddf44a2f3ddffe6fb19
junoon <govilankit86@gmail.com>
2011-01-05 23:00:48 +0530

    Done with Static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -24,4 +31,13 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    # Emulate initializer set_clear_dependencies_hook in 
+    # railties/lib/rails/application/bootstrap.rb
+    ActiveSupport::Dependencies.clear
+  end
 end
+Spork.each_run do
+end
+
